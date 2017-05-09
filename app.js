52f652267bcb805603b9ae1fb3303037aa6ac799
m1 <croxford.miles@gmail.com>
2017-05-09 19:27:04 +0100

    update

--- app.js
+++ app.js
@@ -1,15 +1,87 @@
-const worker  = new Worker('worker.js');
-const content = document.getElementById("content");
+class SumDigits {
+  /**
+   * Parse the given input into finding the next sum of digits
+   *
+   * @param {string} input The input to parse
+   *
+   * @returns {string|int} The next digit with the sum of the input digits,
+   *                       return -1 if not found
+   */
+  parse(input) {
+    this.input     = input;
+    const inputInt = parseInt(this.input, 10);
 
-worker.addEventListener('message', (event) => {
-  content.innerHTML = event.data.msg;
-}, false);
+    if (inputInt === 0 || isNaN(inputInt) || this.input.match(/[a-z]/i)) {
+      return `${this.input} is not a valid number`;
+    }
+
+    if (this.input.length > 14) {
+      return `max input is 14 digits`;
+    }
+
+    // region 1
+    let trailingZeros = 0;
+    let inputIntTemp  = inputInt;
+
+    while (inputIntTemp % 10 === 0) {
+      trailingZeros++;
+      inputIntTemp /= 10;
+    }
+
+    this.region1 = trailingZeros;
+
+    // region 2
+    let inputArrTemp = inputIntTemp.toString().split('');
+    this.region2     = parseInt(inputArrTemp.pop(), 10);
+
+    // region 3
+    let consecutiveNines = 0;
+
+    for (let n of inputArrTemp.reverse()) {
+      if (parseInt(n, 10) === 9) {
+        consecutiveNines++;
+      }
+    }
+
+    this.region3 = consecutiveNines;
+
+    // region 4
+    inputArrTemp.reverse();
+    inputArrTemp = inputArrTemp.splice(0, inputArrTemp.length - consecutiveNines);
+
+    this.region4 = inputArrTemp.length ? parseInt(inputArrTemp.join(""), 10) : 0;
+
+    return this.parseRegions();
+  }
+
+  /**
+   * Parses the regions into the final output
+   *
+   * @returns {string|int} The next digit with the sum of the input digits,
+   *                       return -1 if not found
+   */
+  parseRegions() {
+    let finalRegion = [
+      this.region4 + 1,
+      "0".repeat(this.region1),
+      this.region2 - 1,
+      "9".repeat(this.region3)
+    ];
+
+    finalRegion = finalRegion.filter((n) => {
+      return n !== null && n !== "";
+    }).join("");
+
+    if (finalRegion.length > this.input.length) {
+      return -1;
+    }
+
+    return finalRegion.padStart(this.input.length, "0");
+  }
+}
 
 function search() {
-  const val = document.getElementById('number').value;
+  const sd = new SumDigits();
 
-  if (val !== "") {
-    content.innerHTML = "LOADING";
-    worker.postMessage({'cmd': 'nextDigit', 'number': val});
-  }
+  document.getElementById('content').innerHTML = sd.parse(document.getElementById('number').value);
 }
